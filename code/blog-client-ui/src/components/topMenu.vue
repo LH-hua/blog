5c96360a21d555b21f706e282e6b0bbf2e7ae8e2
huaker <huaker_hk@163.com>
2023-06-09 00:00:08 +0800

    【client】增加登录弹框

--- code/blog-client-ui/src/components/topMenu.vue
+++ code/blog-client-ui/src/components/topMenu.vue
@@ -3,7 +3,7 @@
     <v-avatar class="me-10 ms-4" color="grey-darken-1" size="40">LH</v-avatar>
     <v-spacer></v-spacer>
     <v-btn
-      v-for="item in menu.data"
+      v-for="item in data.menu"
       :key="item.url"
       variant="text"
       @click="onTorouter(item)"
@@ -11,14 +11,33 @@
       <v-icon :icon="item.icon" />
       {{ item.name }}
     </v-btn>
+    <v-avatar
+      style="cursor: pointer"
+      class="me-10 ms-4"
+      color="grey-darken-1"
+      size="45"
+      @click="onDialogShow"
+      >登 录</v-avatar
+    >
+    <v-dialog v-model="data.dialog" persistent width="1024">
+      <v-card>
+        <v-card-text>
+          <v-text-field :counter="10" label="Name" required></v-text-field>
+
+          <v-text-field label="E-mail" required></v-text-field>
+          <v-btn color="#64B5F6"> 登 录 </v-btn>
+          <v-btn @click="onDialogShow" color="#EEEEEE"> 取 消 </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script setup>
 import { reactive } from "vue";
 import { useRouter, useRoute } from "vue-router";
-const menu = reactive({
-  data: [
+const data = reactive({
+  menu: [
     {
       id: "0",
       pid: "0",
@@ -44,14 +63,19 @@ const menu = reactive({
       children: [],
     },
   ],
+  dialog: false,
 });
 
 const router = useRouter();
 const route = useRoute();
 
+function onDialogShow() {
+  data.dialog = !data.dialog;
+}
+
 function onTorouter(obj) {
   console.log(route);
   console.log(obj);
-  router.push(obj.url );
+  router.push(obj.url);
 }
 </script>
