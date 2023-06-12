def3625ce8db70b0f6b19913c078dd99a0ba70b4
huaker <huaker_hk@163.com>
2023-06-12 22:45:15 +0800

    【client】文件删除

--- code/blog-client-ui/src/views/article/index.vue
+++ code/blog-client-ui/src/views/article/index.vue
@@ -1,26 +1,30 @@
 <template>
-    <div>
-      <v-container>
-        <v-row>
-          <v-col cols="2">
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="2" class="">
+          <div style="position: fixed;">
             <card-image></card-image>
-            <br>
+            <br />
             <v-card flat>
-                <v-card-title><v-icon icon="mdi-bullhorn"></v-icon>公告</v-card-title>
-                <v-card-text>
-                    <p>这是LH个人博客，记录和分享日常</p>
-                    <p>有问题欢迎讨论交流</p>
-                </v-card-text>
+              <v-card-title
+                ><v-icon icon="mdi-bullhorn"></v-icon>公告</v-card-title
+              >
+              <v-card-text>
+                <p>这是LH个人博客，记录和分享日常</p>
+                <p>有问题欢迎讨论交流</p>
+              </v-card-text>
             </v-card>
-          </v-col>
-  
-          <v-col>
-            <router-view></router-view>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
-  </template>
+          </div>
+        </v-col>
+
+        <v-col>
+          <router-view></router-view>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
 
 <script setup>
 import cardImage from "@/components/cardImage.vue";
@@ -28,4 +32,3 @@ import cardImage from "@/components/cardImage.vue";
 import { onMounted } from "vue";
 onMounted(() => {});
 </script>
-  
