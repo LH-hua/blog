bf8cc9f3bdced32fe46176bf8fd03495506294f4
huaker <huaker_hk@163.com>
2023-06-24 23:55:22 +0800

    【serve】测试api

--- code/blog-server/src/route/index.js
+++ code/blog-server/src/route/index.js
@@ -1,6 +1,6 @@
 
 module.exports = function (app) {
-  app.use('/post', require('./article'))
-  app.use('/user', require('./user'))
-  app.use('/sys', require('./sys'))
+  app.use('/api/post', require('./article'))
+  app.use('/api/user', require('./user'))
+  app.use('/api/sys', require('./sys'))
 }
