cc9f3fca0d5a55d98e7dd3b9f600326840ad8267
huaker <huaker_hk@163.com>
2023-07-09 23:58:23 +0800

    【client】修复打包后 正式环境访问动态路由失败

--- code/blog-client-ui/src/router/dynamicRoute.js
+++ code/blog-client-ui/src/router/dynamicRoute.js
@@ -6,16 +6,17 @@ export const asyncRoute = async (token) => {
     const res = await userMenuRoute()
     console.log(res)
     const menuArr = res.data.data
-    const modules = import.meta.glob("@/views/*/*.vue", { as: "url" })
+    const modules = import.meta.glob("@/views/*/*.vue",)
     for (const key in modules) {
         modules[key]().then((mod) => {
+            const file = mod.default
             for (let i = 0; i < menuArr.length; i++) {
                 const item = menuArr[i]
                 const componentPath = `/src/views${item.component}`
                 if (key == componentPath) {
                     const route = {
                         path: item.url || item.path,
-                        component: () => import(mod),
+                        component: file,
                     }
                     if (item.pName) {
                         router.addRoute(item.pName,route)
