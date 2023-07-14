5d4f3b37c74af040b3db27b066f01de3cd005a4b
huaker <huaker_hk@163.com>
2023-07-14 23:39:54 +0800

    【server】修复时间显示运行时间

--- code/blog-server/src/controller/article.js
+++ code/blog-server/src/controller/article.js
@@ -7,7 +7,7 @@ const moment = require('moment')
 const article = {
   articles: async (req, res, next) => {
     try {
-      let result = await posts.find({}, { body: 0 }).sort({date:-1})
+      let result = await posts.find({}, { body: 0 }).sort({ date: -1 })
       // console.log(result)
       res.send({
         data: result,
@@ -30,18 +30,24 @@ const article = {
     }
   },
   add: async (req, res) => {
-    const { title, body } = req.body
-    const result = await posts.insertMany({
-      dete: moment().format('YYYY-MM-DD HH:mm:ss'),
-      title: title,
-      body: body
-    })
-    if (result) {
-      res.send({
-        type: 'success',
-        status:200,
-        msg: '文章添加成功了'
+    try {
+      const { title, body } = req.body
+      const date = moment(new Date()).format('YYYY-MM-DD HH:mm:ss')
+      console.log(date)
+      const result = await posts.insertMany({
+        date: date,
+        title: title,
+        body: body
       })
+      if (result) {
+        res.send({
+          type: 'success',
+          status: 200,
+          msg: '文章添加成功了'
+        })
+      }
+    } catch (error) {
+
     }
   }
 }
