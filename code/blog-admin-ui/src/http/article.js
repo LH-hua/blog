7540777a1aae37647673abc3f83c42076d6236d6
LH <huaker.hk@gmail.com>
2024-04-02 23:50:31 +0800

    admin:默认添加api

--- code/blog-admin-ui/src/http/article.js
+++ code/blog-admin-ui/src/http/article.js
@@ -1,6 +1,6 @@
 import { get, post } from './http'
 
-export const article = (p) => get('/post/list', p)
-export const addArticle = (p) => post('/post/findOneAndUpdate', p)
-export const getPostDetal = (p) => get('/post/detail', p)
-export const getCaptcha = (p) => get('post/captcha', p)
+export const article = (p) => get('/api/post/list', p)
+export const addArticle = (p) => post('/api/post/findOneAndUpdate', p)
+export const getPostDetal = (p) => get('/api/post/detail', p)
+export const getCaptcha = (p) => get('/api/post/captcha', p)
