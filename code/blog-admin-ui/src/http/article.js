4b90ae8a73980d953bc1db83b3deb727368c2c62
LH <huaker.hk@gmail.com>
2024-04-02 14:29:26 +0800

    admin:增加一个接口

--- code/blog-admin-ui/src/http/article.js
+++ code/blog-admin-ui/src/http/article.js
@@ -1,6 +1,6 @@
 import { get, post } from './http'
 
 export const article = (p) => get('/post/list', p)
-export const addArticle = (p) => post('/post/create', p)
+export const addArticle = (p) => post('/post/findOneAndUpdate', p)
 export const getPostDetal = (p) => get('/post/detail', p)
 export const getCaptcha = (p) => get('post/captcha', p)
